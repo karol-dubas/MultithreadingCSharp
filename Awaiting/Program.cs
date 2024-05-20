23598a7d0d8eba15b010f7fdfe846f2fc97235b3
Karol Dubas <karol.dubas@primetals.com>
2024-05-20 17:05:49 +0200

    Add await non-blocking example

--- Awaiting/Program.cs
+++ Awaiting/Program.cs
@@ -1,23 +1,25 @@
-﻿var fileRepository = new FileRepository();
-var dbRepository = new DatabaseRepository();
+﻿var repo = new Repository();
 
-var dbResultTask = dbRepository.GetAsync(); // No await, continue
-int fileResult = await fileRepository.GetAsync(); // Again return control to the caller
+var fooTask = repo.GetFooAsync(); // No await, continue
+int barResult = await repo.GetBarAsync(); // Return control to the caller
+int fooResult = await fooTask; // Return control to the caller
 
-class FileRepository
+return barResult + fooResult;
+
+class Repository
 {
-    public async Task<int> GetAsync()
+    public async Task<int> GetFooAsync()
     {
-        Thread.Sleep(500);
+        // Return control to the caller (non-blocking),
+        // it will return here once async operation is finished
+        await Task.Delay(1000);
+        
         return 1;
     }
-}
-
-class DatabaseRepository
-{
-    public async Task<int> GetAsync()
+    
+    public async Task<int> GetBarAsync()
     {
-        await Task.Delay(1000); // Return control to the caller
+        await Task.Delay(500); // Return control to the caller
         return 2;
     }
 }
--- async_notes.md
+++ async_notes.md
@@ -1165,7 +1165,35 @@ await delayTask; // Await first delay, but 500ms already elapsed, so awaiting wi
 // ~1500ms elapsed (500ms + 1000ms)
 ```
 
-`await` returns the control to caller (non-blocking)
+Below is similar example, but illustrating how `await` returns the control to caller (non-blocking).
+
+```cs
+var repo = new Repository();
+
+var fooTask = repo.GetFooAsync(); // No await, continue
+int barResult = await repo.GetBarAsync(); // Return control to the caller
+int fooResult = await fooTask; // Return control to the caller
+
+return barResult + fooResult;
+
+class Repository
+{
+    public async Task<int> GetFooAsync()
+    {
+        // Return control to the caller (non-blocking),
+        // it will return here once async operation is finished
+        await Task.Delay(1000);
+        
+        return 1;
+    }
+    
+    public async Task<int> GetBarAsync()
+    {
+        await Task.Delay(500); // Return control to the caller
+        return 2;
+    }
+}
+```
 
 ================================================================================================
 
