3a24d2cae8a28c951cbcac81d51d2e5ff04747a8
Karol Dubas <karol.dubas@primetals.com>
2024-06-13 16:34:10 +0200

    Add Thread check and Race Conditions

---
+++ CurrentThread/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("[1] Thread:" + Environment.CurrentManagedThreadId);
+
+Task.Run(() =>
+{
+    Console.WriteLine("[2] Thread:" + Environment.CurrentManagedThreadId);
+    
+    Task.Run(() =>
+    {
+        Console.WriteLine("[2.1] Thread:" + Environment.CurrentManagedThreadId);
+    });
+});
+
+Console.WriteLine("[3] Thread:" + Environment.CurrentManagedThreadId);
---
+++ RaceCondidtions/Program.cs
@@ -0,0 +1,15 @@
+﻿// Multiple things performing work on the same shared resource
+
+int number = 0;
+
+List<Task> tasks = [];
+
+for (int i = 0; i < 1000; i++)
+{
+    var task = Task.Run(() => { number++; }); // Race condition
+    tasks.Add(task);
+}
+
+await Task.WhenAll(tasks);
+
+Console.WriteLine(number);
